053f21864359bb755626f270cb9f6eb985e35fae
dannsam <daniil.samoylov@pushpay.com>
2017-08-31 17:42:46 +1200

    Adding `fork-ts-checker-service-before-start` plugin to allow delaying service-start

--- test/integration/index.spec.js
+++ test/integration/index.spec.js
@@ -185,4 +185,24 @@ describe('[INTEGRATION] index', function () {
       createCompiler({ tslint: true });
     }).to.not.throw.error;
   });
+
+  it('should allow delaying service-start', function (callback) {
+    var compiler = createCompiler();
+    var delayed = false;
+
+    compiler.plugin('fork-ts-checker-service-before-start', function (cb) {
+      setTimeout(function () {
+        delayed = true;
+
+        cb();
+      }, 0);
+    });
+
+    compiler.plugin('fork-ts-checker-service-start', function () {
+      expect(delayed).to.be.true;
+      callback();
+    });
+
+    compiler.run(function () {});
+  });
 });

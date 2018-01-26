dbfa7364c0646ddcea86cc1b7bcc755fac4ff252
Iwata Hidetaka <iwata0303@gmail.com>
2018-01-26 17:45:13 +0900

    add tests about vue SFC compilation with each langs

---
+++ test/integration/vue/src/langs/example-jsx.vue
@@ -0,0 +1,11 @@
+<script lang="jsx">
+/* OK */
+import Vue from "vue";
+export default Vue.extend({
+  name: "ExampleJsx",
+  render() {
+    const a = 1; // noUnusedLocals
+    return <div>Example</div>;
+  }
+});
+</script>

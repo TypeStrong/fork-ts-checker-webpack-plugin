aa8d4822c080384d74168bac005ff061522bab01
ktsn <ktsn55@gmail.com>
2018-08-08 10:58:32 +0900

    Avoid false positive of no-consecutive-blank-lines TSLint rule in Vue file

--- test/integration/vue.spec.js
+++ test/integration/vue.spec.js
@@ -145,6 +145,17 @@ describe('[INTEGRATION] vue', function () {
     });
   });
 
+  it('should not report no-consecutive-blank-lines tslint rule', function (callback) {
+    createCompiler({ tslint: true, vue: true });
+
+    compiler.run(function(error, stats) {
+      stats.compilation.warnings.forEach(function(warning) {
+        expect(warning.rawMessage).to.not.match(/no-consecutive-blank-lines/);
+      });
+      callback();
+    });
+  });
+
   it('should find syntactic errors when checkSyntacticErrors is true', function (callback) {
     createCompiler({ tslint: true, vue: true, checkSyntacticErrors: true });
     

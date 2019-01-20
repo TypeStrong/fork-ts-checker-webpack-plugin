2ccecefeebc123c6cfd510d334daa40bf04fcdbb
John Reilly <johnny_reilly@hotmail.com>
2019-01-20 09:34:28 +0000

    more unit tests (#205)
    * replace peerDeps with runtime checks
    
    * update changelog
    
    * make node 6 compatible
    
    * remove dupe / update version
    
    * change engines version
    
    * drop travis from build matrix
    
    * typescript path can now be specified
    
    * update changelog
    
    * fix no implicit dependencies
    
    * add more unit tests
    
    * make tests more specific

--- test/unit/index.spec.js
+++ test/unit/index.spec.js
@@ -1,6 +1,5 @@
 var describe = require('mocha').describe;
 var it = require('mocha').it;
-// var beforeEach = require('mocha').beforeEach;
 var afterEach = require('mocha').afterEach;
 var expect = require('chai').expect;
 var mockRequire = require('mock-require');
@@ -8,6 +7,18 @@ var mockRequire = require('mock-require');
 describe('[UNIT] ForkTsCheckerWebpackPlugin', function() {
   afterEach(function() {
     mockRequire.stopAll();
+  });
+
+  it('should throw if typescript not present', function() {
+    mockRequire('typescript', undefined);
+    var ForkTsCheckerWebpackPlugin = mockRequire.reRequire('../../lib/index');
+
+    expect(function() {
+      new ForkTsCheckerWebpackPlugin();
+    }).to.throw(
+      Error,
+      'When you use this plugin you must install `typescript`.'
+    );
   });
 
   it('should not throw if typescript version >= 2.1.0', function() {
@@ -25,7 +36,23 @@ describe('[UNIT] ForkTsCheckerWebpackPlugin', function() {
 
     expect(function() {
       new ForkTsCheckerWebpackPlugin();
-    }).to.throw();
+    }).to.throw(
+      Error,
+      'Cannot use current typescript version of 2.0.8, the minimum required version is 2.1.0'
+    );
+  });
+
+  it('should throw if tslint not present', function() {
+    mockRequire('typescript', { version: '2.1.0' });
+    mockRequire('tslint', undefined);
+    var ForkTsCheckerWebpackPlugin = mockRequire.reRequire('../../lib/index');
+
+    expect(function() {
+      new ForkTsCheckerWebpackPlugin({ tslint: true });
+    }).to.throw(
+      Error,
+      'When you use `tslint` option, make sure to install `tslint`.'
+    );
   });
 
   it('should not throw if tslint version >= 4.0.0', function() {
@@ -45,6 +72,9 @@ describe('[UNIT] ForkTsCheckerWebpackPlugin', function() {
 
     expect(function() {
       new ForkTsCheckerWebpackPlugin({ tslint: true });
-    }).to.throw();
+    }).to.throw(
+      Error,
+      'Cannot use current tslint version of 3.15.1, the minimum required version is 4.0.0'
+    );
   });
 });

940b5e982425e16b80d2139c0de71124b0b99312
John Koutsoumpas <empire@adslgr.com>
2019-04-20 19:03:44 +0300

    Change the watch mode of TS compiler to 'UseFsEventsWithFallbackDynamicPolling'
    - add typescriptWatchMode option
    - add pre-defined enum 'ForkTsCheckerWebpackPlugin.TypescriptWatchMode' that consists of TS 3.4.x supported values for watch mode
    - set the environment variable 'TSC_WATCHFILE' according to the options before the creation and execution of the compiler program
    - append used TS Incremental API and TS watch mode settings to the information output
    - update README.md with help about this new option

--- src/index.ts
+++ src/index.ts
@@ -30,6 +30,15 @@ interface Logger {
   info(message?: any): void;
 }
 
+enum TypescriptWatchMode {
+  DEFAULT = 'UseFsEventsWithFallbackDynamicPolling',
+  PriorityPollingInterval = 'PriorityPollingInterval',
+  DynamicPriorityPolling = 'DynamicPriorityPolling',
+  UseFsEvents = 'UseFsEvents',
+  UseFsEventsWithFallbackDynamicPolling = 'UseFsEventsWithFallbackDynamicPolling',
+  UseFsEventsOnParentDirectory = 'UseFsEventsOnParentDirectory'
+}
+
 interface Options {
   typescript: string;
   tsconfig: string;
@@ -53,6 +62,7 @@ interface Options {
   vue: boolean;
   useTypescriptIncrementalApi: boolean;
   measureCompilationTime: boolean;
+  typescriptWatchMode: TypescriptWatchMode;
   resolveModuleNameModule: string;
   resolveTypeReferenceDirectiveModule: string;
 }
@@ -81,6 +91,8 @@ class ForkTsCheckerWebpackPlugin {
     return getForkTsCheckerWebpackPluginHooks(compiler);
   }
 
+  public static readonly TypescriptWatchMode = TypescriptWatchMode;
+
   public readonly options: Partial<Options>;
   private tsconfig: string;
   private compilerOptions: object;
@@ -101,6 +113,7 @@ class ForkTsCheckerWebpackPlugin {
   private colors: Chalk;
   private formatter: Formatter;
   private useTypescriptIncrementalApi: boolean;
+  private typescriptWatchMode: TypescriptWatchMode;
   private resolveModuleNameModule: string | undefined;
   private resolveTypeReferenceDirectiveModule: string | undefined;
 
@@ -224,6 +237,14 @@ class ForkTsCheckerWebpackPlugin {
       options.useTypescriptIncrementalApi === undefined
         ? semver.gte(this.typescriptVersion, '3.0.0') && !this.vue
         : options.useTypescriptIncrementalApi;
+
+    this.typescriptWatchMode =
+      options.typescriptWatchMode !== undefined
+        ? options.typescriptWatchMode
+        : ForkTsCheckerWebpackPlugin.TypescriptWatchMode.DEFAULT;
+
+    // set the environment variable for the watch mode of typescript here
+    process.env.TSC_WATCHFILE = this.typescriptWatchMode.toString();
 
     this.measureTime = options.measureCompilationTime === true;
     if (this.measureTime) {
@@ -662,7 +683,14 @@ class ForkTsCheckerWebpackPlugin {
           ) +
           ' with ' +
           this.colors.bold(this.memoryLimit + 'MB') +
-          ' memory limit'
+          ' memory limit' +
+          ' and ' +
+          (this.useTypescriptIncrementalApi === true
+            ? this.colors.bold('Experimental TS Incremental API compilation') +
+              ' in ' +
+              this.colors.bold(this.typescriptWatchMode) +
+              ' watch mode'
+            : this.colors.bold('Normal Incremental compilation'))
       );
 
       if (this.watchPaths.length && this.isWatching) {

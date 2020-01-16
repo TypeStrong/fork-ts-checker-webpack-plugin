3d9be03ee92cbc2f11e2bd403a5c78f241efd6ba
Piotr Oleś <piotrek.oles@gmail.com>
2020-01-16 21:50:12 +0100

    chore: upgrade semantic-release to the stable version

---
+++ release.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  branches: ['master', 'beta'],
+  plugins: [
+    '@semantic-release/commit-analyzer',
+    '@semantic-release/release-notes-generator',
+    '@semantic-release/npm',
+    '@semantic-release/github'
+  ]
+};

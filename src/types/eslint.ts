e2c1583f46beae3ff96d6ac00ba036d9ffc3b39f
Nathan Bierema <nbierema@gmail.com>
2020-03-01 15:46:57 -0500

    feat: support eslint fix

--- src/types/eslint.ts
+++ src/types/eslint.ts
@@ -34,3 +34,65 @@ export interface LintReport {
   fixableErrorCount: number;
   fixableWarningCount: number;
 }
+
+export interface Options {
+  allowInlineConfig?: boolean;
+  baseConfig?: false | { [name: string]: any };
+  cache?: boolean;
+  cacheFile?: string;
+  cacheLocation?: string;
+  configFile?: string;
+  cwd?: string;
+  envs?: string[];
+  errorOnUnmatchedPattern?: boolean;
+  extensions?: string[];
+  fix?: boolean;
+  globals?: string[];
+  ignore?: boolean;
+  ignorePath?: string;
+  ignorePattern?: string | string[];
+  useEslintrc?: boolean;
+  parser?: string;
+  parserOptions?: ParserOptions;
+  plugins?: string[];
+  resolvePluginsRelativeTo?: string;
+  rules?: {
+    [name: string]: RuleLevel | RuleLevelAndOptions;
+  };
+  rulePaths?: string[];
+  reportUnusedDisableDirectives?: boolean;
+}
+
+interface ParserOptions {
+  ecmaVersion?:
+    | 3
+    | 5
+    | 6
+    | 7
+    | 8
+    | 9
+    | 10
+    | 11
+    | 2015
+    | 2016
+    | 2017
+    | 2018
+    | 2019
+    | 2020;
+  sourceType?: 'script' | 'module';
+  ecmaFeatures?: {
+    globalReturn?: boolean;
+    impliedStrict?: boolean;
+    jsx?: boolean;
+    experimentalObjectRestSpread?: boolean;
+    [key: string]: any;
+  };
+  [key: string]: any;
+}
+
+type RuleLevel = Severity | 'off' | 'warn' | 'error';
+type Severity = 0 | 1 | 2;
+
+interface RuleLevelAndOptions extends Array<any> {
+  0: RuleLevel;
+}

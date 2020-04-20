b4e92daff6f6a616bc5bba7b9e615d870ca12793
Piotr Oleś <piotrek.oles@gmail.com>
2020-04-20 19:16:44 +0200

    test: modify tests to be able to run them on typescript 2.7.1

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,10 @@
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
-  setupFilesAfterEnv: ['./test/setup.ts']
+  setupFilesAfterEnv: ['./test/setup.ts'],
+  globals: {
+    'ts-jest': {
+      tsConfig: './test/tsconfig.json'
+    },
+  },
 };

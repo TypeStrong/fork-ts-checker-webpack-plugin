07c31ef2b40df2a5a2fe39af79feb07073ce2108
Piotr Oleś <piotrek.oles@gmail.com>
2020-05-16 17:32:06 +0200

    chore: update semantic-release configuration (#410)
    Release new patch versions for "refactor" and "docs" commits.

--- release.config.js
+++ release.config.js
@@ -7,7 +7,21 @@ module.exports = {
     }
   ],
   plugins: [
-    '@semantic-release/commit-analyzer',
+    [
+      '@semantic-release/commit-analyzer',
+      {
+        preset: 'angular',
+        releaseRules: [
+          { breaking: true, release: 'major' },
+          { revert: true, release: 'patch' },
+          { type: 'feat', release: 'minor' },
+          { type: 'fix', release: 'patch' },
+          { type: 'perf', release: 'patch' },
+          { type: 'refactor', release: 'patch' },
+          { type: 'docs', release: 'patch' }
+        ]
+      }
+    ],
     '@semantic-release/release-notes-generator',
     '@semantic-release/npm',
     '@semantic-release/github'

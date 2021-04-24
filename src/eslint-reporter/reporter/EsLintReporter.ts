747edcb5c70f2bd0eda6ade5fbf3e3814df85c96
Piotr Oleś <piotrek.oles@gmail.com>
2021-04-24 23:32:45 +0200

    fix: resolve eslint dependencies for proper watching
    ✅ Closes: #580

--- src/eslint-reporter/reporter/EsLintReporter.ts
+++ src/eslint-reporter/reporter/EsLintReporter.ts
@@ -3,7 +3,9 @@ import { CLIEngine, LintReport, LintResult } from '../types/eslint';
 import { createIssuesFromEsLintResults } from '../issue/EsLintIssueFactory';
 import { EsLintReporterConfiguration } from '../EsLintReporterConfiguration';
 import { Reporter } from '../../reporter';
+import { normalize } from 'path';
 import minimatch from 'minimatch';
+import glob from 'glob';
 
 function createEsLintReporter(configuration: EsLintReporterConfiguration): Reporter {
   // eslint-disable-next-line @typescript-eslint/no-var-requires
@@ -11,46 +13,98 @@ function createEsLintReporter(configuration: EsLintReporterConfiguration): Repor
   const engine: CLIEngine = new CLIEngine(configuration.options);
 
   let isInitialRun = true;
+  let isInitialGetFiles = true;
+
   const lintResults = new Map<string, LintResult>();
-  const includedFilesPatterns = engine.resolveFileGlobPatterns(configuration.files);
+  const includedGlobPatterns = engine.resolveFileGlobPatterns(configuration.files);
+  const includedFiles = new Set<string>();
+
+  function isFileIncluded(path: string) {
+    return (
+      includedGlobPatterns.some((pattern) => minimatch(path, pattern)) &&
+      !engine.isPathIgnored(path)
+    );
+  }
+
+  async function getFiles() {
+    if (isInitialGetFiles) {
+      isInitialGetFiles = false;
+
+      const resolvedGlobs = await Promise.all(
+        includedGlobPatterns.map(
+          (globPattern) =>
+            new Promise<string[]>((resolve) => {
+              glob(globPattern, (error, resolvedFiles) => {
+                if (error) {
+                  // fail silently
+                  resolve([]);
+                } else {
+                  resolve(resolvedFiles || []);
+                }
+              });
+            })
+        )
+      );
+
+      for (const resolvedGlob of resolvedGlobs) {
+        for (const resolvedFile of resolvedGlob) {
+          if (isFileIncluded(resolvedFile)) {
+            includedFiles.add(resolvedFile);
+          }
+        }
+      }
+    }
+
+    return Array.from(includedFiles);
+  }
+
+  function getDirs() {
+    return includedGlobPatterns || [];
+  }
+
+  function getExtensions() {
+    return configuration.options.extensions || [];
+  }
 
   return {
     getReport: async ({ changedFiles = [], deletedFiles = [] }) => {
       return {
         async getDependencies() {
+          for (const changedFile of changedFiles) {
+            if (isFileIncluded(changedFile)) {
+              includedFiles.add(changedFile);
+            }
+          }
+          for (const deletedFile of deletedFiles) {
+            includedFiles.delete(deletedFile);
+          }
+
           return {
-            files: [],
-            dirs: [],
-            extensions: [],
+            files: (await getFiles()).map((file) => normalize(file)),
+            dirs: getDirs().map((dir) => normalize(dir)),
+            extensions: getExtensions(),
           };
         },
         async getIssues() {
           // cleanup old results
-          changedFiles.forEach((changedFile) => {
+          for (const changedFile of changedFiles) {
             lintResults.delete(changedFile);
-          });
-          deletedFiles.forEach((removedFile) => {
-            lintResults.delete(removedFile);
-          });
+          }
+          for (const deletedFile of deletedFiles) {
+            lintResults.delete(deletedFile);
+          }
 
           // get reports
           const lintReports: LintReport[] = [];
 
           if (isInitialRun) {
-            lintReports.push(engine.executeOnFiles(includedFilesPatterns));
+            lintReports.push(engine.executeOnFiles(includedGlobPatterns));
             isInitialRun = false;
           } else {
             // we need to take care to not lint files that are not included by the configuration.
             // the eslint engine will not exclude them automatically
-            const changedAndIncludedFiles = changedFiles.filter(
-              (changedFile) =>
-                includedFilesPatterns.some((includedFilesPattern) =>
-                  minimatch(changedFile, includedFilesPattern)
-                ) &&
-                (configuration.options.extensions || []).some((extension) =>
-                  changedFile.endsWith(extension)
-                ) &&
-                !engine.isPathIgnored(changedFile)
+            const changedAndIncludedFiles = changedFiles.filter((changedFile) =>
+              isFileIncluded(changedFile)
             );
 
             if (changedAndIncludedFiles.length) {
@@ -64,11 +118,11 @@ function createEsLintReporter(configuration: EsLintReporterConfiguration): Repor
           }
 
           // store results
-          lintReports.forEach((lintReport) => {
-            lintReport.results.forEach((lintResult) => {
+          for (const lintReport of lintReports) {
+            for (const lintResult of lintReport.results) {
               lintResults.set(lintResult.filePath, lintResult);
-            });
-          });
+            }
+          }
 
           // get actual list of previous and current reports
           const results = Array.from(lintResults.values());

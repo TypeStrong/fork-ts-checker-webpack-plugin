066081037ff382f1ff3e738d81367179897a1d4e
Piotr Oleś <piotrek.oles@gmail.com>
2022-01-31 21:26:09 +0100

    chore: update changelog config

--- changelog.config.js
+++ changelog.config.js
@@ -7,30 +7,37 @@ module.exports = {
     feat: {
       description: 'A new feature',
       value: 'feat',
+      section: 'Features',
     },
     fix: {
       description: 'A bug fix',
       value: 'fix',
+      section: 'Bug Fixes',
     },
     refactor: {
       description: 'A code change that neither adds a feature or fixes a bug',
       value: 'refactor',
+      hidden: true,
     },
     perf: {
       description: 'A code change that improves performance',
       value: 'perf',
+      hidden: true,
     },
     test: {
       description: 'Adding missing tests',
       value: 'test',
+      hidden: true,
     },
     chore: {
       description: 'Build process, CI or auxiliary tool changes',
       value: 'chore',
+      hidden: true,
     },
     docs: {
       description: 'Documentation only changes',
       value: 'docs',
+      hidden: true,
     },
   },
 };

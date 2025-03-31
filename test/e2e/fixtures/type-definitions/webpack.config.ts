a8d693900685bd623752431fdbdd65098ca6e58a
Piotr Oleś <piotr.oles@datadoghq.com>
2025-03-31 08:11:11 +0200

    test: use different hash function because of ssl breaking change

--- test/e2e/fixtures/type-definitions/webpack.config.ts
+++ test/e2e/fixtures/type-definitions/webpack.config.ts
@@ -2,6 +2,9 @@ import ForkTsCheckerWebpackPlugin from 'fork-ts-checker-webpack-plugin';
 
 const config = {
   entry: './src/index.ts',
+  output: {
+    hashFunction: 'xxhash64', // @see https://stackoverflow.com/questions/69692842/error-message-error0308010cdigital-envelope-routinesunsupported
+  },
   plugins: [
     new ForkTsCheckerWebpackPlugin({
       async: 'invalid_value',
